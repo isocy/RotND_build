82e62468b7f016dea0d4e6ecc47f1cfc3f5a2e68
isocy <isocy13576@kaist.ac.kr>
2025-01-03 14:06:45 +0900

    fix minor

--- RotND_build/Global/__init__.py
+++ RotND_build/Global/__init__.py
@@ -1,14 +1,14 @@
 from Global.const_def import *
 
-RAW_BEATMAP_PATH = OVERTHINKER_HARD_PATH
+RAW_BEATMAP_PATH = GLASS_CAGES_HARD_PATH
 
-ONE_VIBE_START_BEATS_EXCEPT: list[float] = [345.5]
-TWO_VIBES_START_BEATS_EXCEPT: list[float] = [282]
+ONE_VIBE_START_BEATS_EXCEPT: list[float] = [76.5, 139.5]
+TWO_VIBES_START_BEATS_EXCEPT: list[float] = [241.5, 242.5]
 THREE_VIBES_START_BEATS_EXCEPT: list[float] = []
 
-ONE_VIBE_START_BEATS_LOOSE: list[tuple[float, int]] = [(117.5, 1)]
+ONE_VIBE_START_BEATS_LOOSE: list[tuple[float, int]] = []
 TWO_VIBES_START_BEATS_LOOSE: list[tuple[float, int]] = []
 THREE_VIBES_START_BEATS_LOOSE: list[tuple[float, int]] = []
 
-TARGET_PARTITION: tuple[int, ...] = ()
-GREAT_START_BEATS: list[tuple[float, int]] = []
+TARGET_PARTITION: tuple[int, ...] = (1, 1, 2)
+GREAT_START_BEATS: list[tuple[float, int]] = [(77.5, 1)]
